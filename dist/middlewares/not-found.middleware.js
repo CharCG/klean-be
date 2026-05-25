b7375bbb008f7eaab8ba432a238480c09559c476
Charles <charlescong91@gmail.com>
2026-05-25 19:50:30 +0700

    chore: remove dist

--- dist/middlewares/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundMiddleware = void 0;
-const error_util_1 = require("../utils/error.util");
-const notFoundMiddleware = (req, res, next) => {
-    throw new error_util_1.NotFoundError();
-};
-exports.notFoundMiddleware = notFoundMiddleware;
--- dist/schemas/report.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createReportSchema = void 0;
-const zod_1 = require("zod");
-exports.createReportSchema = zod_1.z.object({
-    issue: zod_1.z.string().min(1),
-});
